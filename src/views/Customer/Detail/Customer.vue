8e065708bc063899bef0f1cefcd454ae0c179b6a
Everton Ferreira <everton.ferreira@shiddo.com.br>
2024-01-11 21:39:11 -0300

    feat: Adicionando a tela de listagem de clientes e a de criação
    Adicionando a tela de listagem de clientes e a de criação

---
+++ src/views/Customer/Detail/Customer.vue
@@ -0,0 +1,157 @@
+<!-- eslint-disable arrow-parens -->
+<template>
+  <a-card title="Cadastro de Cliente" :bordered="false">
+    <a-row class="flex justify-center">
+      <a-col :xs="{ span: 24, offset: 0 }" :lg="{ span: 12, offset: 6 }">
+        <a-form @finish="submitForm" :model="formState">
+          <a-form-item
+            label="Nome do Cliente"
+            name="name"
+            :rules="[{ required: true, message: 'Campo nome do cliente é obrigatório!' }]"
+          >
+            <a-input v-model:value="formState.name" placeholder="Digite seu nome" maxLenght="120" />
+          </a-form-item>
+          <a-form-item
+            label="Documento"
+            name="document"
+            :rules="[{ required: true, message: 'Campo documento é obrigatório!' }]"
+          >
+            <a-input v-model:value="formState.document" placeholder="Digite seu documento" />
+          </a-form-item>
+          <a-form-item label="Telefone">
+            <a-input
+              v-model:value="formState.phone"
+              placeholder="Digite seu telefone"
+              maxLenght="14"
+            />
+          </a-form-item>
+          <a-form-item label="E-mail">
+            <a-input
+              v-model:value="formState.email"
+              maxLenght="140"
+              placeholder="Digite seu e-mail"
+              type="email"
+            />
+          </a-form-item>
+          <a-form-item label="Cliente ativo?">
+            <a-switch
+              v-model:checked="formState.active"
+              checked-children="Sim"
+              un-checked-children="Não"
+            />
+          </a-form-item>
+          <a-form-item>
+            <div class="flex justify-items-center w-full footer-form-container">
+              <a-button type="primary" htmlType="submit">
+                {{ consumerId ? 'Editar' : 'Criar' }}
+              </a-button>
+              <a-button style="margin-left: 10px" @click="handlerCancel">Cancelar</a-button>
+            </div>
+          </a-form-item>
+        </a-form>
+      </a-col>
+    </a-row>
+  </a-card>
+</template>
+
+<script lang="ts">
+import { onMounted, ref } from 'vue';
+import Services, { ICustomer } from '@/services';
+import { useRoute } from 'vue-router';
+// eslint-disable-next-line import/no-cycle
+import useCustomer from '@/hooks/useCustomer';
+// eslint-disable-next-line import/no-cycle
+import router from '@/router';
+
+interface FormState {
+  name: string;
+  document: string;
+  phone: string;
+  email: string;
+  active: boolean;
+}
+
+export default {
+  name: 'CustomerView',
+  setup() {
+    const route = useRoute();
+    const { id } = route.params;
+
+    const { createCustomer, updateCustomer } = useCustomer();
+
+    const initialValues = {
+      name: '',
+      document: '',
+      phone: '',
+      email: '',
+      active: false,
+    };
+
+    const formState = ref<FormState>(initialValues);
+
+    const handlerCancel = () => {
+      router.back();
+    };
+
+    onMounted(() => {
+      if (id) {
+        Services.getCustomerById(id)
+          .then(({ data }: { data: ICustomer }) => {
+            formState.value = data;
+          })
+          // eslint-disable-next-line arrow-parens
+          .catch(error => console.log(error));
+      }
+    });
+
+    const submitForm = () => {
+      if (!id) {
+        createCustomer(formState.value, () => {
+          formState.value = initialValues;
+        });
+      } else {
+        updateCustomer(formState.value, () => {
+          formState.value = initialValues;
+        });
+      }
+    };
+    return {
+      consumerId: id,
+      formState,
+      submitForm,
+      handlerCancel,
+    };
+  },
+};
+</script>
+
+<style>
+.ant-form-item-explain-error {
+  text-align: justify;
+  margin: 0 12px;
+}
+
+.footer-form-container {
+  display: flex;
+  width: 100%;
+  justify-content: center;
+}
+
+.ant-form-item-control-input-content {
+  display: flex;
+}
+
+.ant-card {
+  max-width: 90%;
+  margin: 2rem auto;
+}
+
+.ant-card-head {
+  text-align: center;
+}
+
+label {
+  min-width: 125px;
+  margin-right: 1rem;
+}
+</style>
---
+++ src/views/Customer/List/Customer.vue
@@ -0,0 +1,155 @@
+<!-- eslint-disable vuejs-accessibility/click-events-have-key-events -->
+<template>
+  <a-card title="Lista de Produtos" :bordered="false">
+    <a-row>
+      <a-col :xs="{ span: 24, offset: 0 }" :lg="{ span: 14, offset: 6 }">
+        <a-table :columns="columns" bordered :dataSource="dataSource" :rowKey="'id'">
+          <template #bodyCell="{ column, record }">
+            <template v-if="column.key === 'active'">
+              {{ record.active ? 'Sim' : 'Não' }}
+            </template>
+
+            <template v-if="column.key === 'action'">
+              <a-dropdown-button>
+                <template #overlay>
+                  <a-menu>
+                    <a-menu-item key="1" @click="routerEditPage(record.id)">
+                      <EditOutlined />
+                      Editar
+                    </a-menu-item>
+                    <a-menu-item key="2">
+                      <SwapOutlined />
+                      <a-popconfirm
+                        title="Deseja realmente alterar o estado desse produto?"
+                        @confirm="updateClick(record)"
+                      >
+                        <a>{{ record.active ? 'Desativar' : 'Ativar' }}</a>
+                      </a-popconfirm>
+                    </a-menu-item>
+                    <a-menu-item key="3" @click="routerAssocientePage(record.id)">
+                      <DiffOutlined />
+                      Associar Produtos
+                    </a-menu-item>
+                  </a-menu>
+                </template>
+              </a-dropdown-button>
+            </template>
+          </template>
+        </a-table>
+      </a-col>
+    </a-row>
+  </a-card>
+</template>
+
+<script lang="ts">
+import { defineComponent, onMounted, ref } from 'vue';
+import { ICustomer } from '@/services';
+
+// eslint-disable-next-line import/no-cycle
+import useCustomer from '@/hooks/useCustomer';
+
+import { EditOutlined, SwapOutlined, DiffOutlined } from '@ant-design/icons-vue';
+
+const dataSource = ref<ICustomer[]>([]);
+
+export default defineComponent({
+  name: 'CustomerList',
+  components: {
+    EditOutlined,
+    SwapOutlined,
+    DiffOutlined,
+  },
+  setup() {
+    // eslint-disable-next-line object-curly-newline, operator-linebreak
+    const { getAllCustomers, routerAssocientePage, routerEditPage, updateActiveCustomers } =
+      useCustomer();
+
+    const updateClick = (row: ICustomer) => {
+      const newRow = row;
+      newRow.active = !row.active;
+      updateActiveCustomers(row, () => {
+        getAllCustomers((data: ICustomer[]) => {
+          dataSource.value = data;
+        });
+      });
+    };
+
+    onMounted(() => {
+      getAllCustomers((data: ICustomer[]) => {
+        dataSource.value = data;
+      });
+    });
+
+    return {
+      dataSource,
+      updateClick,
+      routerAssocientePage,
+      routerEditPage,
+
+      columns: [
+        {
+          title: 'Name',
+          dataIndex: 'name',
+          key: 'name',
+        },
+        {
+          title: 'E-mail',
+          dataIndex: 'email',
+          key: 'email',
+        },
+        {
+          title: 'Documento',
+          dataIndex: 'document',
+          key: 'document',
+        },
+        {
+          title: 'Telefone',
+          dataIndex: 'phone',
+          key: 'phone',
+        },
+        {
+          title: 'Ativo',
+          dataIndex: 'active',
+          key: 'active',
+        },
+        {
+          title: 'Address',
+          dataIndex: 'action',
+          key: 'action',
+          fixed: 'center',
+          width: 200,
+        },
+      ],
+    };
+  },
+});
+</script>
+
+<style lang="scss">
+.ant-table-wrapper {
+  overflow-x: auto;
+}
+.ant-card-head {
+  text-align: center;
+}
+.ant-card {
+  max-width: 90%;
+  margin: 2rem auto;
+}
+
+.ant-btn-group {
+  button:first-child {
+    display: none;
+  }
+}
+
+.ant-dropdown-trigger {
+  border-radius: 6px;
+}
+
+.ant-dropdown-menu-title-content {
+  svg {
+    margin-right: 6px;
+  }
+}
+</style>
