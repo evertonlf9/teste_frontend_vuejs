cc18ce9dd3a5869c0bf690c2fb3f2fd358eca449
Everton Ferreira <everton.ferreira@shiddo.com.br>
2024-01-11 21:37:48 -0300

    feat: Adicionando a tela de lista gem de produtos e a de criação
    Adicionando a tela de lista gem de produtos e a de criação

---
+++ src/views/Products/Detail/Product.vue
@@ -0,0 +1,145 @@
+<!-- eslint-disable vuejs-accessibility/label-has-for -->
+<template>
+  <a-card title="Cadastro de Produtos" :bordered="false">
+    <a-row>
+      <a-col :xs="{ span: 24, offset: 0 }" :lg="{ span: 12, offset: 6 }">
+        <a-form @finish="submitForm" :model="formState">
+          <a-form-item
+            label="Nome do produto"
+            name="name"
+            :rules="[{ required: true, message: 'Campo nome do produto é obrigatório!' }]"
+          >
+            <a-input v-model:value="formState.name" />
+          </a-form-item>
+          <a-form-item class="flex" label="Produto ativo?">
+            <a-switch
+              class="gap-5 left-3"
+              v-model:checked="formState.active"
+              checked-children="Sim"
+              un-checked-children="Não"
+            />
+          </a-form-item>
+          <a-form-item>
+            <div class="flex justify-items-center w-full footer-form-container">
+              <a-button type="primary" htmlType="submit">
+                {{ productId ? 'Editar' : 'Criar' }}
+              </a-button>
+              <a-button style="margin-left: 10px" @click="handlerCancel">Cancelar</a-button>
+            </div>
+          </a-form-item>
+        </a-form>
+      </a-col>
+    </a-row>
+  </a-card>
+</template>
+
+<script lang="ts">
+import { defineComponent, onMounted, ref } from 'vue';
+import { IProduct } from '@/services';
+import useProducts from '@/hooks/useProducts';
+// eslint-disable-next-line import/no-cycle
+import router from '@/router';
+import { useRoute } from 'vue-router';
+
+interface FormState {
+  id?: string;
+  name: string;
+  active: boolean;
+}
+
+export default defineComponent({
+  name: 'ProductDetail',
+  setup() {
+    const route = useRoute();
+    const { id } = route.params;
+
+    const { createProduct, updateProduct, getProductById } = useProducts();
+
+    const formState = ref<FormState>({
+      id: '',
+      name: '',
+      active: false,
+    });
+
+    const handlerCancel = () => {
+      router.back();
+    };
+
+    const submitForm = () => {
+      const payload = {
+        id: formState.value.id,
+        name: formState.value.name,
+        active: formState.value.active,
+      };
+
+      if (!id) {
+        createProduct(payload, () => {
+          formState.value.name = '';
+          formState.value.active = false;
+        });
+      } else {
+        updateProduct(payload, () => {
+          formState.value.name = '';
+          formState.value.active = false;
+        });
+      }
+    };
+
+    onMounted(() => {
+      if (id) {
+        getProductById(id, (data: IProduct) => {
+          formState.value = data;
+        });
+      }
+    });
+
+    return {
+      productId: id,
+      options: [
+        {
+          value: 'sim',
+          label: 'Sim',
+        },
+        {
+          value: 'não',
+          label: 'Não',
+        },
+      ],
+      formState,
+      submitForm,
+      handlerCancel,
+    };
+  },
+});
+</script>
+
+<style>
+.ant-form-item-control-input-content {
+  display: flex;
+}
+
+.footer-form-container {
+  display: flex;
+  width: 100%;
+  justify-content: center;
+}
+
+.ant-form-item-explain-error {
+  text-align: justify;
+  margin: 0 12px;
+}
+
+.ant-card {
+  max-width: 90%;
+  margin: 2rem auto;
+}
+
+.ant-card-head {
+  text-align: center;
+}
+
+label {
+  min-width: 125px;
+  margin-right: 1rem;
+}
+</style>
---
+++ src/views/Products/List/Product.vue
@@ -0,0 +1,137 @@
+<!-- eslint-disable vuejs-accessibility/click-events-have-key-events -->
+<template>
+  <a-card title="Lista de Produtos" :bordered="false">
+    <a-row>
+      <a-col :xs="{ span: 24, offset: 0 }" :lg="{ span: 12, offset: 6 }">
+        <a-table :columns="columns" bordered :dataSource="dataSource" :rowKey="'id'">
+          <template #bodyCell="{ column, record }">
+            <template v-if="column.key === 'active'">
+              {{ record.active ? 'Sim' : 'Não' }}
+            </template>
+
+            <template v-if="column.key === 'action'">
+              <a-dropdown-button>
+                <template #overlay>
+                  <a-menu>
+                    <a-menu-item key="1" @click="editClick(record.id)">
+                      <EditOutlined />
+                      Editar
+                    </a-menu-item>
+                    <a-menu-item key="2">
+                      <SwapOutlined />
+                      <a-popconfirm
+                        title="Deseja realmente alterar o estado desse produto?"
+                        @confirm="updateClick(record)"
+                      >
+                        <a>{{ record.active ? 'Desativar' : 'Ativar' }}</a>
+                      </a-popconfirm>
+                    </a-menu-item>
+                  </a-menu>
+                </template>
+              </a-dropdown-button>
+            </template>
+          </template>
+        </a-table>
+      </a-col>
+    </a-row>
+  </a-card>
+</template>
+
+<script lang="ts">
+import { defineComponent, onMounted, ref } from 'vue';
+import { IProduct } from '@/services';
+import useProducts from '@/hooks/useProducts';
+import { EditOutlined, SwapOutlined } from '@ant-design/icons-vue';
+// eslint-disable-next-line import/no-cycle
+import router from '@/router';
+
+const dataSource = ref<IProduct[]>([]);
+
+export default defineComponent({
+  name: 'ProductList',
+  components: {
+    EditOutlined,
+    SwapOutlined,
+  },
+  setup() {
+    const { getAllProducts, updateStatusProduct } = useProducts();
+
+    const updateClick = (row: IProduct) => {
+      const newRow = row;
+      newRow.active = !row.active;
+      updateStatusProduct(newRow, () => {
+        getAllProducts((data: IProduct[]) => {
+          dataSource.value = data;
+        });
+      });
+    };
+
+    const editClick = (id: string) => {
+      router.push(`/editar-produto/${id}`);
+    };
+
+    onMounted(() => {
+      getAllProducts((data: IProduct[]) => {
+        dataSource.value = data;
+      });
+    });
+
+    return {
+      dataSource,
+      updateClick,
+      editClick,
+
+      columns: [
+        {
+          title: 'Name',
+          dataIndex: 'name',
+          key: 'name',
+        },
+        {
+          title: 'Ativo',
+          dataIndex: 'active',
+          key: 'active',
+        },
+        {
+          title: 'Address',
+          dataIndex: 'action',
+          key: 'action',
+          fixed: 'center',
+          width: 200,
+        },
+      ],
+    };
+  },
+});
+</script>
+
+<style lang="scss">
+.ant-table-wrapper {
+  overflow-x: auto;
+}
+
+.ant-card-head {
+  text-align: center;
+}
+
+.ant-card {
+  max-width: 90%;
+  margin: 2rem auto;
+}
+
+.ant-btn-group {
+  button:first-child {
+    display: none;
+  }
+}
+
+.ant-dropdown-trigger {
+  border-radius: 6px;
+}
+
+.ant-dropdown-menu-title-content {
+  svg {
+    margin-right: 6px;
+  }
+}
+</style>
